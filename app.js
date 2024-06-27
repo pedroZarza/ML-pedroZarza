dca1e144d0293878f0a6c4e4530ae9cd5df2830a
pedroZarza <pedrozarza08@gmail.com>
2024-06-27 20:50:52 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app  = express();
+const path = require("path");
+
+
+
+
+app.use(express.static("public"));
+
+let port = 3000;
+
+app.listen(port, ()=>{
+    console.log("servidor corriendo en puerto "+ port);
+})
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve("./views/home.html"));
+})
+
+
+app.get("/register", (req, res)=>{
+    res.sendFile(path.resolve("./views/register.html"));
+})
+
+
+app.get("/login", (req, res)=>{
+    res.sendFile(path.resolve("./views/login.html"));
+})
